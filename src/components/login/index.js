17f672d5293e2937fd78bc1e67bd8ab29da8fd6b
Morten Røstad <mortenrostad@hotmail.com>
2020-11-05 15:44:29 +0100

    signup.sss, navbar, login

---
+++ src/components/login/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useFirebaseApp } from 'reactfire';
+import 'firebase/auth';
+import './login.css';
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    error: '',
+  });
+
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+      error: ''
+    })
+  };
+
+  const firebase = useFirebaseApp();
+
+  //kogs in user
+  const userLogin = e => {
+    e.preventDefault();
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password).catch(error => {
+      //finds potential errors
+      setUser({
+        ...user,
+        error: error.message,
+      })
+    })
+  }
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      window.location = '/'; //After successful login, user will be redirected to home.html
+    }
+  });
+
+  return (
+    <>
+      <div id="registerbox"><h1 class="login">Log In</h1>
+        <form class="form" onSubmit={userLogin}>
+          <input type="text" placeholder="Email" name="email" onChange={handleChange} /><br />
+          <input type="password" placeholder="Password" name="password" onChange={handleChange} /><br />
+          <div><button type="submit">Log in</button></div>
+        </form>
+        {user.error && <h4>{user.error}</h4>}
+        <div class="a"> Not registered? <a href="/register">Create an Account!</a></div>
+      </div>
+    </>
+  )
+};
+
+export default Login;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAxM264qthYVjnwJPcMJNYu2nueBAmpY_4",
+  authDomain: "web-iii-a91c8.firebaseapp.com",
+  databaseURL: "https://web-iii-a91c8.firebaseio.com",
+  projectId: "web-iii-a91c8",
+  storageBucket: "web-iii-a91c8.appspot.com",
+  messagingSenderId: "853961489045",
+  appId: "1:853961489045:web:789a743d228cc0b252b99c",
+  measurementId: "G-T9LRESEJ1M"
+});
+
+
+let db = firebase.firestore()
+
+
+export default {
+  firebase, db
+}
