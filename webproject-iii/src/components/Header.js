4e53db61b6efe99064e1c414b2c15bf2096932b6
Isabell <isabell_h_j@hotmail.com>
2020-11-13 16:12:10 +0100

    css edit placement on links

--- webproject-iii/src/components/Header.js
+++ webproject-iii/src/components/Header.js
@@ -12,10 +12,10 @@ function Header() {
           <nav>
           <a href="/">Home</a>
           {/* displays logout, newlisting log in and profile link depending on user is logged in or logged out. */}
+          {user && <Logout/>}    
+          {!user && <a href="/login">Login</a>}
           {user && <a href="/profile">Profile</a>}
           {user && <a href="/newlisting"> <FaPlus /> New Listing</a>}
-          {user && <Logout/>}
-          {!user && <a href="/login">login</a>}
           </nav>
       </header>
   )
