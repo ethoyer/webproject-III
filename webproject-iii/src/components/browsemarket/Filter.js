b31ab2bf9017fee7e224ee124ef86cd5b846eecd
elisabet <hoyerelisabet@gmail.com>
2020-11-16 15:01:59 +0100

    css /browsemarket

--- webproject-iii/src/components/browsemarket/Filter.js
+++ webproject-iii/src/components/browsemarket/Filter.js
@@ -7,27 +7,28 @@ class Filter extends React.Component {
     this.state = { categoryFilter: null };
   }
 
-  changeCategory(e){
-const newChosenCategory = document.getElementById('filterCategory').value;
-this.props.categoryChangeThroughFilter(newChosenCategory);
+  changeCategory(e) {
+    const newChosenCategory = document.getElementById('filterCategory').value;
+    this.props.categoryChangeThroughFilter(newChosenCategory);
   }
 
-  componentDidMount(){
-document.getElementById('filterCategory').value = this.props.filter;
+  componentDidMount() {
+    document.getElementById('filterCategory').value = this.props.filter;
   }
-  
-  render(){
-  return (
-    <>
-    <p>Filter:</p>
-    <label htmlFor="filterCategory">Category:</label>
-          <select id="filterCategory" name="filterCategory" onChange={(e) => this.changeCategory(e)}>
-            <option value="housing">Housing</option>
-            <option value="books_and_supplies">Books and supplies</option>
-            <option value="furniture">Furniture</option>
-            <option value="services">Services</option>
-          </select>
-    </>
-  )
-}}
+
+  render() {
+    return (
+      <div id="browsemarketFilter">
+        <p>Filter</p>
+        <label htmlFor="filterCategory">Category:</label>
+        <select id="filterCategory" name="filterCategory" onChange={(e) => this.changeCategory(e)}>
+          <option value="housing">Housing</option>
+          <option value="books_and_supplies">Books and supplies</option>
+          <option value="furniture">Furniture</option>
+          <option value="services">Services</option>
+        </select>
+      </div>
+    )
+  }
+}
 export default Filter;
