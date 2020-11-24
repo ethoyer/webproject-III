4a1c2131b1dff2d9b91c8fbc42781b4a2ff838c6
elisabet <hoyerelisabet@gmail.com>
2020-11-24 13:10:25 +0100

    forbereder mappe til innlevering

--- webproject-iii/src/index.js
+++ webproject-iii/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { FirebaseAppProvider } from 'reactfire';
 import firebaseConfig from './firebaseConfig';
-import * as serviceWorker from './serviceWorker';
 import App from './App'; //  importer componenten som heter App fra filen app.js  se app.js for flere notater
 import Header from './components/Header.js';
 import Footer from './components/Footer';
@@ -22,5 +21,3 @@ ReactDOM.render(
   </FirebaseAppProvider>,
   document.getElementById('root') //renderer <app> i strictmode, og plasserer App komponenten i #root(root ligger inne i ../public/index.html)
 );
-
-serviceWorker.unregister();
